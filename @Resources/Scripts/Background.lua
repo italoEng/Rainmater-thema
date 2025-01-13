e9b3efa3fd0fc51c6bd9dab82754992c2881d5a6
italoEng <franca.italo05@gmail.com>
2025-01-12 23:31:32 -0300

    Size

--- @Resources/Scripts/Background.lua
+++ @Resources/Scripts/Background.lua
@@ -1,21 +1,29 @@
 function Initialize()
-    -- Inicialização do script (se necessário)
+    -- Inicialização do script
 end
 
-function ToggleBackground(option)
-    local background = SKIN:GetVariable("background" .. option)
-    if not background  then
-        print("background não encontrada para a opção: " .. option)
+function B(option)
+    -- Determina a cor com base na opção selecionada
+    local optionValue = SKIN:GetVariable("optionValue" .. option)
+    if not optionValue then
+        print("Cor não encontrada para a opção: " .. option)
         return
     end
 
+    -- Lista de skins para atualizar
     local skins = {
-        {name = "CPU", path = "#ROOTCONFIGPATH#CPU\\CPU.ini"}
+        {name = "Settings", path = "#ROOTCONFIGPATH#Settings\\Settings.ini"},
+        {name = "CPU", path = "#ROOTCONFIGPATH#CPU\\CPU.ini"},
+        {name = "RAM", path = "#ROOTCONFIGPATH#RAM\\RAM.ini"},
+        {name = "GPU", path = "#ROOTCONFIGPATH#GPU\\GPU.ini"},
+        {name = "GPU", path = "#ROOTCONFIGPATH#\\CircularBar.ini"}
+
     }
 
     -- Atualiza cada skin na lista
     for _, skin in ipairs(skins) do
-        SKIN:Bang("!WriteKeyValue", "Variables", "ShowBackground", color, skin.path)
+        SKIN:Bang("!WriteKeyValue", "Variables", "SelectedOption", optionValue, skin.path)
         SKIN:Bang("!Refresh", "Fbz001\\" .. skin.name)
     end
 end
+
